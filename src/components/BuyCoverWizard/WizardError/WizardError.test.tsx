c4f5d7a16440aaa9009876460fb2e8a1734a88b6
Rahul Shetty <Rahul.Shetty@nike.com>
2022-07-19 20:20:20 +0530

    Add test cases for wizard error and pitch

--- src/components/BuyCoverWizard/WizardError/WizardError.test.tsx
+++ src/components/BuyCoverWizard/WizardError/WizardError.test.tsx
@@ -1,9 +1,29 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import WizardError from './WizardError'
 
 describe('WizardError suite', () => {
   test('renders without crashing', () => {
     const { container } = render(<WizardError onClick={() => null} />)
     expect(container).toBeTruthy()
+  })
+
+  test('renders with the correct text', () => {
+    render(<WizardError onClick={() => null} />)
+
+    expect(screen.getByText('Ooops!')).toBeTruthy()
+    expect(
+      screen.getByText(
+        'Your age is over our accepted limit. We are sorry but we cannot insure you now.'
+      )
+    ).toBeTruthy()
+    expect(screen.getByText('Ok :(')).toBeTruthy()
+  })
+
+  test('calls onClick when the button is clicked', () => {
+    const onClick = jest.fn()
+    render(<WizardError onClick={onClick} />)
+
+    screen.getByText('Ok :(').click()
+    expect(onClick).toHaveBeenCalled()
   })
 })
--- src/components/BuyCoverWizard/WizardPitch/WizardPitch.test.tsx
+++ src/components/BuyCoverWizard/WizardPitch/WizardPitch.test.tsx
@@ -1,9 +1,29 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import WizardPitch from './WizardPitch'
 
 describe('WizardPitch suite', () => {
   test('renders without crashing', () => {
     const { container } = render(<WizardPitch onClick={() => null} />)
     expect(container).toBeTruthy()
+  })
+
+  test('renders with the correct text', () => {
+    render(<WizardPitch onClick={() => null} />)
+
+    expect(screen.getByText('Hello There!')).toBeTruthy()
+    expect(
+      screen.getByText(
+        "Let's buy some insurance. It is going to take only a few steps"
+      )
+    ).toBeTruthy()
+    expect(screen.getByText('Start')).toBeTruthy()
+  })
+
+  test('calls onClick when the button is clicked', () => {
+    const onClick = jest.fn()
+    render(<WizardPitch onClick={onClick} />)
+
+    screen.getByText('Start').click()
+    expect(onClick).toHaveBeenCalled()
   })
 })
