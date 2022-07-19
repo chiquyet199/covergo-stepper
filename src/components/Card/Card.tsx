dd16f7d309f7fb89634be921704c9286a68893b9
Rahul Shetty <Rahul.Shetty@nike.com>
2022-07-19 23:55:40 +0530

    Style the card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,11 @@
+import styles from './Card.module.css'
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
 }
 
-const Card: React.FC<CardProps> = ({ children, ...props }) => {
+const Card: React.FC<CardProps> = ({ children, className, ...props }) => {
   return (
-    <div className="card" {...props}>
+    <div className={`${styles.cardContainer} ${className}`} {...props}>
       {children}
     </div>
   )
