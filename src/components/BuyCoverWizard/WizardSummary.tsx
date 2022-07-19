7010dc345835717edfe71105240aa2558d719c7b
Rahul Shetty <Rahul.Shetty@nike.com>
2022-07-19 14:06:40 +0530

    Create Wizard Summary

---
+++ src/components/BuyCoverWizard/WizardSummary.tsx
@@ -0,0 +1,14 @@
+const WizardSummary = () => {
+  return (
+    <div>
+      <h2>Summary</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
+        nisl eget consectetur sagittis, nisl nunc egestas nisi, euismod aliquam
+        nisl nisl eget nisi.
+      </p>
+    </div>
+  )
+}
+
+export default WizardSummary
