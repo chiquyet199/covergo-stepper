edf1f7edc366fccc13fd748bcf6f33d5336906fe
Rahul Shetty <Rahul.Shetty@nike.com>
2022-07-20 16:29:05 +0530

    Add more test cases for button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,24 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Button from './Button'
 
 describe('Button suite', () => {
   test('renders without crashing', () => {
     const { container } = render(<Button>Save</Button>)
     expect(container).toBeTruthy()
+  })
+
+  test('renders with the correct text', () => {
+    render(<Button>Save</Button>)
+
+    expect(screen.getByText('Save')).toBeTruthy()
+  })
+
+  test('calls onClick when the button is clicked', () => {
+    const onClick = jest.fn()
+    render(<Button onClick={onClick}>Save</Button>)
+
+    userEvent.click(screen.getByText('Save'))
+    expect(onClick).toHaveBeenCalled()
   })
 })
